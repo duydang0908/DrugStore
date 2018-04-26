047cb2bf4c6be3d3fff020c7ee74a161d8b84519
duydang0908 <38183058+duydang0908@users.noreply.github.com>
2018-04-26 21:37:39 +0700

    up-source

---
+++ MVCDrugStoreManager/MVCDrugStoreManager/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDrugStoreManager.Models;
+
+namespace MVCDrugStoreManager.Controllers
+{
+    public class AdminController : Controller
+    {
+        dbDrugStoreManagerDataContext db = new dbDrugStoreManagerDataContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Trang Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // Trang Details
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
