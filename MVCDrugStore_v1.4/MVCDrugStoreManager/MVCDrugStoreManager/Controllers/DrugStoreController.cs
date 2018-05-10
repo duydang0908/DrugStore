82bb333f7c93c0b7a836b5c6d49a5a73e3498659
CookieAnk <cookieank1781997@gmail.com>
2018-05-10 22:16:18 +0700

    MVCDrugStore
    Sửa layout và thêm 1 vài chi tiết

---
+++ MVCDrugStore_v1.4/MVCDrugStoreManager/MVCDrugStoreManager/Controllers/DrugStoreController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDrugStoreManager.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace MVCDrugStoreManager.Controllers
+{
+
+    public class DrugStoreController : Controller
+    {
+        dbDrugStoreManagerDataContext db = new dbDrugStoreManagerDataContext();
+        // GET: DrugStore
+        private List<SAN_PHAM> Layspmoi(int count)
+        {
+            return db.SAN_PHAMs.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+
+        //  Trang index
+        public ActionResult Index(int ? page, string searchString)
+        {
+            var sp = from e in db.SAN_PHAMs select e;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                sp = sp.Where(s => s.TenSP.Contains(searchString));
+            }
+            ViewBag.SeachString = searchString;
+            //tạo bien so trang
+            int pageNumber = (page ?? 1);
+            //tao bien quy dinh so san pham tren moi trang
+            int pageSize = 8; 
+            return View(sp.ToList().OrderBy(n => n.MaSP).ToPagedList(pageNumber, pageSize));
+            //return View(db.SAN_PHAMs.ToList());
+        }
+
+        public ActionResult loaiSP()
+        {
+            var cate = from cd in db.LOAI_SAN_PHAMs select cd;
+            return PartialView(cate);
+        }
+
+        public ActionResult spTheoLoai(int id)
+        {
+            var product = from s in db.SAN_PHAMs where s.MaLoaiSP == id select s;
+            return View(product);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var product = from s in db.SAN_PHAMs
+                          where s.MaSP == id
+                          select s;
+            return View(product.Single());
+        }
+    }
+}
