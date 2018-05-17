ac1f06235554416fb55ef19f9b047f14209ddaa0
CookieAnk <cookieank1781997@gmail.com>
2018-05-18 01:58:50 +0700

    MVC
    Xem lại đơn hàng của khách hàng

---
+++ MVCDrugStore_v1.5.1/MVCDrugStoreManager/MVCDrugStoreManager/Controllers/UserController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDrugStoreManager.Models;
+namespace MVCDrugStoreManager.Controllers
+{
+    public class UserController : Controller
+    {
+        dbDrugStoreManagerDataContext db = new dbDrugStoreManagerDataContext();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Đăng ký
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Dangky(FormCollection collection,KHACH_HANG kh)
+        {
+            var hoten = collection["HotenKH"];
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            var matkhaunhaplai = collection["Matkhaunhaplai"];
+            var diachi = collection["Diachi"];
+            var email = collection["Email"];
+            var dienthoai = collection["Dienthoai"];
+            if (String.IsNullOrEmpty(hoten))
+            {
+                ViewData["Loi1"] = "Họ tên khách hàng không được để trống";
+            }
+            else if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi2"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi3"] = "Phải nhập mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(matkhaunhaplai) || matkhaunhaplai != matkhau)
+            {
+                ViewData["Loi4"] = "Phải nhập lại mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(email))
+            {
+                ViewData["Loi5"] = "Email không được bỏ trống";
+            }
+            else if (String.IsNullOrEmpty(dienthoai))
+            {
+                ViewData["Loi6"] = "Phải nhập điện thoại";
+            }
+            else
+            {
+                kh.TenKH = hoten;
+                kh.IDLogin = tendn;
+                kh.Pass = matkhau;
+                kh.Email = email;
+                kh.DiaChi = diachi;
+                kh.SDT = dienthoai;
+                db.KHACH_HANGs.InsertOnSubmit(kh);
+                db.SubmitChanges();
+                return RedirectToAction("Dangnhap");
+            }
+            return this.DangKy();
+        }
+
+
+        // Đăng nhập
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection collection)
+        {
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                KHACH_HANG kh = db.KHACH_HANGs.SingleOrDefault(n => n.IDLogin == tendn && n.Pass == matkhau);
+                if (kh != null)
+                {
+                    //ViewBag.Thongbao = "Chúc mừng đăng nhập thành công";
+                    Session["Taikhoan"] = kh;
+                    Session["HoTen"] = kh.TenKH;
+                    return RedirectToAction("GioHang", "GioHang");
+                }
+                else
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+            }
+            return View();
+        }
+
+        //Trang đăng xuất
+        public ActionResult DangXuat()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "DrugStore");
+        }
+    }
+}
---
+++ MVCDrugStore_v1.5.1/MVCDrugStoreManager/MVCDrugStoreManager/Models/Giohang.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVCDrugStoreManager.Models;
+
+namespace MVCDrugStoreManager.Models
+{
+    public class Giohang
+    {
+        dbDrugStoreManagerDataContext data = new dbDrugStoreManagerDataContext();
+        public int iMaSP { get; set; }
+        public string sTenSP  { get; set; }
+        public string sHinh{ get; set; }
+        public int iSoLuong { get; set; }
+        public double dDonGia { get; set; }
+        public double dThanhTien
+        { 
+            get { return iSoLuong * dDonGia; }
+        }
+
+        public Giohang(int MaSP)
+        {
+            iMaSP = MaSP;
+            SAN_PHAM sp = data.SAN_PHAMs.Single(n => n.MaSP == iMaSP);
+            sTenSP = sp.TenSP;
+            sHinh = sp.Images;
+            dDonGia = double.Parse(sp.DonGia.ToString());
+            iSoLuong = 1;
+        }
+    }
+}
